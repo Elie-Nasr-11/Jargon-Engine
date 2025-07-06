dfc174e8d04e1e13858a37ae8e55fe351af4320a
Elie-Nasr-11 <88428381+Elie-Nasr-11@users.noreply.github.com>
2025-07-06 15:33:15 +0300

    Update AskException.py

--- AskException.py
+++ AskException.py
@@ -2,3 +2,4 @@ class AskException(Exception):
     def __init__(self, prompt: str, variable: str):
         self.prompt = prompt
         self.variable = variable
+        super().__init__(f"ASK required: {prompt} as {variable}")
