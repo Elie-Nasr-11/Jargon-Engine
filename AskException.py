93262b7cf0cf59ee5a04c388105919b7ea555379
Elie-Nasr-11 <88428381+Elie-Nasr-11@users.noreply.github.com>
2025-07-06 15:32:01 +0300

    Update AskException.py

--- AskException.py
+++ AskException.py
@@ -2,4 +2,3 @@ class AskException(Exception):
     def __init__(self, prompt: str, variable: str):
         self.prompt = prompt
         self.variable = variable
-        super().__init__(f"ASK triggered: {prompt} → {variable}")
